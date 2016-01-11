7ce23092dd8dca171bf849d8c675a0d27ebcdfec
YK <kawashima.human@gmail.com>
2016-01-11 13:35:28 +0900

    no message

--- client/app.js
+++ client/app.js
@@ -5,14 +5,19 @@ var Todo = function (data) {
   }
   
 var vm = {
-  init: function () {
-    vm.list = m.prop([])
+  init : function () {
+    vm.list = Todo.list()
     vm.description = m.prop("")
     vm.add = function () {
       if (vm.description()){
         vm.list().push(new Todo({description: vm.description()}))
         vm.description("")
+        Todo.save(vm.list())
         }
+      }
+    vm.check = function (value){
+      this.done(value)
+      Todo.save(vm.list())
       }
     }
    }
@@ -28,17 +33,51 @@ function view(){
                 }),
     m("button", { onclick: vm.add}, "追加"),
     m("table", vm.list().map(function(task){
-      console.log(1) 
       return m("tr", [ 
          m("td", [
-          m("input[type=checkbox]", { onclick: m.withAttr("checked", task.done), 
-                                       value: task.done()})]),
+          m("input[type=checkbox]", { onclick: m.withAttr("checked", vm.check.bind(task)), 
+                                      value: task.done()})]),
          m("td", { style: { textDecoration: task.done() ? "line-through" : "none"}},
            task.description()
          )
        ])
       }))
   ])
+  }
+
+Todo.list = function () {
+  return m.request({
+    method: "GET", 
+    url: "/tasks",
+    type: Todo
+    })
+/*
+  var tasks = []
+  var src = localStorage.getItem("todo")
+  if (src) {
+    var json = JSON.parse(src)
+    for (i=0;i<json.length;i++) {
+      tasks.push(new Todo(json[i]))
+      }
+    }
+    return m.prop(tasks)
+*/  
+  }
+
+Todo.save = function (todoList){
+  var data = todoList.filter(function(todo){
+    return !todo.done()
+    })
+  m.request({
+    method: "post",
+    url: "/tasks",
+    data: data
+    })
+/*
+  localStorage.setItem("todo",JSON.stringify(todoList.filter(function(todo){
+    return !todo.done()
+    })))
+*/
   }
 
   m.mount(document.getElementById('root'),
